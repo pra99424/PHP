f16f2a6b20697fd613c237bd5514558710620722
pra99424 <pradeep99424@gmail.com>
2023-03-13 18:03:56 +0530

    13.03.2023

--- Errors/error.php
+++ Errors/error.php
@@ -8,6 +8,16 @@
 </head>
 
 <body>
+<?php
+$file=fopen("mytestfile.txt","r");
+?> 
+ <?php
+if(file_exists("mytestfile.txt")) {
+  $file = fopen("mytestfile.txt", "r");
+} else {
+  die("Error: The file does not exist.");
+}
+?> 
     
 
 </body>
